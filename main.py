82ca64bb9a6ca6491f7f57eebcd7f44c3177d87e
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-22 13:11:23 +0700

    refactor: update imports and migrate missions to external module

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import kernel, setup
-import pid_lib as P
-import field as F
-from menu import select_mission
+import sys
+# Make sure the system folder can be resolved if needed, though standard packages should work
+from system import kernel, setup
+from system import pid_lib as P
+from system import field as F
+from system.menu import select_mission
 
-async def mission_1(odo, tm):
-    print("--- Running Mission 1 ---")
-    
-    # Use coordinates from field.py
-    await P.straight(odo, 50)
-    
-    await P.turn(odo, F.TARGET_OBSTACLE_1[2])
-    await P.goto_xy(odo, F.TARGET_OBSTACLE_1[0], F.TARGET_OBSTACLE_1[1])
-    
-
-async def mission_2(odo, tm):
-    print("--- Running Mission 2 ---")
-    await P.straight(odo, -20)
+# Import missions
+from missions import mission_1, mission_2
 
 # Main Mission Handler
 async def main_mission(odo, tm):
     # 1. Show Menu to select mission
-    missions = [mission_1, mission_2]
+    missions = [mission_1.run, mission_2.run]
     selected_idx = await select_mission(missions)
     
     # 2. Run the selected mission
